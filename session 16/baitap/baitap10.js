0ee9e19cf4a4a1b3bfcbf253e999f3d57c0cc4cd
pthongnhung <pthongnhung13122006@gmail.com>
2025-02-26 11:43:45 +0700

    btvn

--- session 16/baitap/baitap10.js
+++ session 16/baitap/baitap10.js
@@ -1,4 +1,11 @@
-let firtNumber = prompt("Moi ban nhap so thu 1: ");
-let secondNumber = prompt("Moi ban nhap so thu 2: ");
-let numRandom = Math.random(firtNumber, secondNumber);
+let firstNumber = parseFloat(prompt("nhap so thu 1: "));
+let secondNumber = parseFloat(prompt("nhap so thu 2: "));
+
+if (firstNumber > secondNumber) {
+    let temp = firstNumber;
+    firstNumber = secondNumber;
+    secondNumber = temp;
+}
+
+let numRandom = Math.random() * (secondNumber - firstNumber) + firstNumber;
 console.log(numRandom);
